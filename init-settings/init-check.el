18a7ef90bbaa4365f9d4ae85d78873e71cd2418c
Francesco Cadei <francesco.cadei@tuta.io>
2024-09-18 22:23:00 +0200

    Using single dictionary.

--- init-settings/init-check.el
+++ init-settings/init-check.el
@@ -4,10 +4,6 @@
 
 (require 'ispell)
 (setq ispell-program-name "hunspell")
-(setq ispell-dictionary "en_US,it_IT")
-(ispell-set-spellchecker-params)
-(ispell-hunspell-add-multi-dic "en_US,it_IT")
-(add-hook 'text-mode-hook 'flyspell-mode)
 
 (use-package flycheck
   :config
