a5af4670b7be1e8394a78ee9894fac52f4f51fbe
Francesco Cadei <francesco.cadei@tuta.io>
2024-09-30 11:53:26 +0200

    Setted typescript.

--- init-languages/init-mp.el
+++ init-languages/init-mp.el
@@ -2,8 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package json-mode)
+(use-package typescript-mode)
+
 (use-package eglot)
 (add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'typescript-mode-hook 'eglot-ensure)
 
 (provide 'init-mp)
 ;;; init-mp.el ends here
